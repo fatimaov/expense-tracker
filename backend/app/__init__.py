f23e227781ab70196f61cb51b47da1d64c138d0d
Fatima Olea <232623523+fatimaov@users.noreply.github.com>
2026-06-28 21:45:37 +0200

    chore: set up SQLAlchemy, Migrate and JWT extensions

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,12 +1,18 @@
 from flask import Flask
 
 from .config import Config
+from .extensions import db, jwt, migrate
 from .routes import api
 
 
 def create_app() -> Flask:
     app = Flask(__name__)
     app.config.from_object(Config)
+
+    db.init_app(app)
+    migrate.init_app(app, db)
+    jwt.init_app(app)
+
     app.register_blueprint(api)
 
     return app
