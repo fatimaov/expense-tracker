97845b58a10e4f0ee34d5dfd3cad2772f2750293
Fatima Olea <232623523+fatimaov@users.noreply.github.com>
2026-09-04 17:23:17 +0200

    add deleted non demo users step

--- .github/scripts/demo_reset.py
+++ .github/scripts/demo_reset.py
@@ -17,3 +17,41 @@
 # if email succeeds, workflow ends normally
 # ↓
 # if email fails, reset still stays done but email error is logged
+
+import requests
+
+from utils import get_required_env, connect_to_database, send_confirmation_email
+
+demo_email = "demo@email.com"
+
+def delete_non_demo_users(conn):
+    with conn.cursor() as cur:
+        cur.execute(
+            """
+            DELETE FROM users
+            WHERE email != %s;
+            """,
+            (demo_email,),
+        )
+    conn.commit()
+
+
+def main():
+    print("Starting demo-reset workflow...")
+
+    database_url = get_required_env("SUPABASE_DATABASE_URL")
+    resend_api_key = get_required_env("RESEND_API_KEY")
+    email_from = get_required_env("EMAIL_FROM")
+    email_to = get_required_env("EMAIL_TO")
+
+    print("Environment variables loaded successfully.")
+
+    with connect_to_database(database_url) as conn:
+        print("Database connection successful.")
+
+        delete_non_demo_users(conn)
+        print("All users except demo@email.com deleted successfully")
+
+
+if __name__ == "__main__":
+    main()
